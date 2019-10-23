ebce83db3b65e6980f5edbbd59a4b243385deafa
Farz Bhullar <farz@codegaragetech.com>
2019-10-23 17:47:28 +0530

    3 Plan creation.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,4 @@
- User.create!(:email => "admin@gmail.com", :password => 'admin1234', :role => 'admin')
+ # User.create!(:email => "admin@gmail.com", :password => 'admin1234', :role => 'admin')
+ Plan.create(:name => "6 Months", :limit_to_buy => 1, :duration => '6 Months')
+ Plan.create(:name => "1 Year", :limit_to_buy => 1, :duration => '2 Year')
+ Plan.create(:name => "2 Year", :limit_to_buy => 1, :duration => '2 Year')
